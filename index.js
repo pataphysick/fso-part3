b74c27daaac8045cd262dc1c6161e314376b80ed
pataphysick <qfwfq@pataphysick.org>
2024-04-16 23:02:10 -0700

    Exercise 3.5

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
-const people = [
+
+let people = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -27,6 +29,58 @@ const people = [
 
 app.get('/api/persons', (request, response) => {
   response.json(people)
+})
+
+app.get('/info', (request, response) => {
+  const n = people.length
+  const datetime = new Date().toString()
+
+  response.send(`<p>Phonebook has info for ${n} people.</p><p>${datetime}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = people.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  people = people.filter(person => person.id !== id)
+
+  response.status(204).end()
+
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
+    })
+  }
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 1000000) + 1
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  people = people.concat(person)
+
+  response.json(people)
+
 })
 
 const PORT = 3001
