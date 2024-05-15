dd525eb70aaf2674b2c1a4d383d357de822081a6
pataphysick <qfwfq@pataphysick.org>
2024-05-14 19:48:41 -0700

    Exercise 3.16

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 morgan.token('content', (req, res) => {return JSON.stringify(req.body)})
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :content`))
@@ -59,9 +59,10 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
         .then(result => response.status(204).end())
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -92,6 +93,14 @@ app.post('/api/persons', (request, response) => {
 })
 
 
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'Malformed ID'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
