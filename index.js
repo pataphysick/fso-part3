c8f0407d317affdd4d6c5592c929328aa1676875
pataphysick <qfwfq@pataphysick.org>
2024-05-16 19:29:05 -0700

    Exercise 3.19

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -83,6 +83,7 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save().then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -93,7 +94,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
         .then(updatedPerson => {
           console.log(updatedPerson)
           response.json(updatedPerson)
@@ -105,6 +106,9 @@ app.put('/api/persons/:id', (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'Malformed ID'})
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
   next(error)
 }
