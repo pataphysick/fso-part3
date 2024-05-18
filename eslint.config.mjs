4923c0c001fc629d3a33938f123ae05e33c38d85
pataphysick <qfwfq@pataphysick.org>
2024-05-18 14:27:54 -0700

    Exercise 3.22 pre-fix

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js"
+
+
+export default [
+  {ignores: ["dist/"]},
+  {files: ["**/*.js"],
+   languageOptions: {sourceType: "script", ecmaVersion: "latest"},
+   plugins: {"@stylistic/js": stylisticJs},
+   rules: {
+     '@stylistic/js/indent': ['error', 2],
+     '@stylistic/js/linebreak-style': ['error', 'unix'],
+     '@stylistic/js/quotes': ['error', 'single'],
+     '@stylistic/js/semi': ['error', 'never'],
+     'eqeqeq': 'error',
+     'no-trailing-spaces': 'error',
+     'object-curly-spacing': ['error', 'always'],
+     'arrow-spacing': ['error', {'before': true, 'after': true}],
+     'no-console': 0
+   }
+  },
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended
+];
